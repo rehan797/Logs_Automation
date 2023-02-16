ec5f179f0838793fd00a9d817f1ee690922219e0
rehan797 <113163920+rehan797@users.noreply.github.com>
2023-02-16 09:58:19 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -3,13 +3,12 @@ tar -cvf /root/logs/"httpd-logs-$timestamp.tar" /var/log/nginx
 
 aws s3 cp /root/logs/httpd-logs-${timestamp}.tar  s3://s3bucket-144
 
-if [ ! -f /root/logs/inventory.html ]
+if [ ! -f /var/www/html/inventory.html ]
 then
-                echo -e "Log Type\tTime Created\tType\tSize" > /root/logs/inventory.html
-                echo -e "httpd-logs\t$timestamp\ttar\t`du -sh /root/logs/httpd-logs-$timestamp.tar | awk '{print $1}'` " >> /root/logs/inventory.html
-        else
-                echo -e "httpd-logs\t$timestamp\ttar\t`du -sh /root/logs/httpd-logs-$timestamp.tar | awk '{print $1}'` " >> /root/logs/inventory.html
-
+        echo -e "Log Type\tTime Created\tType\tSize" >/var/www/html/inventory.html
+        echo -e "httpd-logs\t$timestamp\ttar\t`du -sh /var/www/html/inventory.html | awk '{print $1}'` " >> /var/www/html/inventory.html
+else
+        echo -e "httpd-logs\t$timestamp\ttar\t`du -sh /var/www/html/inventory.html | awk '{print $1}'` " >> /var/www/html/inventory.html
 fi
 
 rm -f /root/logs/httpd-logs-$timestamp.tar
